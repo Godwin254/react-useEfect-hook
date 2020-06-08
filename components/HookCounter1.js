435147fa6e77b5473b1b63551f4e9503c0d7cc2d
Godwin254 <godwinbee15@gmail.com>
2020-06-08 15:42:35 -0400

    1 file modified

--- components/HookCounter1.js
+++ components/HookCounter1.js
@@ -6,6 +6,12 @@ function HookCounter1(){
 
   const [count, setCounter] = useState(0);
   const [name, setName] = useState('');
+
+  useEffect(() => {
+    console.log('-useEffect-updating document title')
+    document.title = `You clicked ${count} times`
+  }, [count])
+
   return (
 
     <div>
