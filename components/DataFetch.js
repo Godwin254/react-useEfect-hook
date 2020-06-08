3250ad0306bc7770578df5e61791c64dfebde8f0
Godwin254 <godwinbee15@gmail.com>
2020-06-08 17:36:33 -0400

    defined jsx

---
+++ components/DataFetch.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react'
+
+function DataFetch(){
+
+  return (
+    <div>
+      <button type='button'>next post</button>
+       <button type='button'>prev post</button>
+    </div>
+  )
+}
