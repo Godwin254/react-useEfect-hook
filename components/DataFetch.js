6631312820eee03f638a97cff4f3af047e67be4e
Godwin254 <godwinbee15@gmail.com>
2020-06-08 17:40:32 -0400

    import axios

--- components/DataFetch.js
+++ components/DataFetch.js
@@ -1,11 +1,22 @@
 import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function DataFetch(){
+
+  const [post, setPost] = useState({})
+  const [count, setCount] = useState(1)
+
+  useEffect(() => {
+    
+  })
 
   return (
     <div>
       <button type='button'>next post</button>
-       <button type='button'>prev post</button>
+      <button type='button'>prev post</button>
+      <div>
+      
+      </div>
     </div>
   )
 }
