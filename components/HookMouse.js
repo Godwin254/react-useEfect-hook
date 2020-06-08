e475e5edb57bcc96c0d5168d15b33d1bfe4d33a3
Godwin254 <godwinbee15@gmail.com>
2020-06-08 16:13:30 -0400

    defined jsx

---
+++ components/HookMouse.js
@@ -0,0 +1,12 @@
+//Run effect only once
+
+import React, {useState, useEffect} from 'react'
+
+function HookMouse() {
+
+  return (
+    <div>
+      HOOK-MOUSE-POSITION X -{} Y-{}
+    </div>
+  )
+}
