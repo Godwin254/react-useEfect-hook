58bc0b21870fd7b729a3e9d2db704b6c0b601b58
Godwin254 <godwinbee15@gmail.com>
2020-06-08 16:18:24 -0400

    logMousePostion method to capture position of mouse

--- components/HookMouse.js
+++ components/HookMouse.js
@@ -5,6 +5,12 @@ import React, {useState, useEffect} from 'react'
 function HookMouse() {
   const [X, setX] = useState(0)
   const [Y, setY] = useState(0)
+
+  const logMousePosition = e =>{
+    console.log('-useEffect- called')
+    setX(e.clientX);
+    setY(e.clientY);
+  }
   return (
     <div>
       HOOK-MOUSE-POSITION X -{X} Y-{Y}
