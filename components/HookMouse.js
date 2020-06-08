9985f5dff5a540df5878000ab924cd7341e63a39
Godwin254 <godwinbee15@gmail.com>
2020-06-08 16:15:03 -0400

    initialize useState function

--- components/HookMouse.js
+++ components/HookMouse.js
@@ -3,10 +3,11 @@
 import React, {useState, useEffect} from 'react'
 
 function HookMouse() {
-
+  const [X, setX] = useState(0)
+  const [Y, setY] = useState(0)
   return (
     <div>
-      HOOK-MOUSE-POSITION X -{} Y-{}
+      HOOK-MOUSE-POSITION X -{X} Y-{Y}
     </div>
   )
 }
