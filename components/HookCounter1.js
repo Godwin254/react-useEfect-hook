2cdc705b13bcb8b5338846014f8b54b9f88bd2b8
Godwin254 <godwinbee15@gmail.com>
2020-06-08 15:39:14 -0400

    initialize useState function

--- components/HookCounter1.js
+++ components/HookCounter1.js
@@ -3,8 +3,11 @@
 import React, {useEffect, useState} from 'react'
 
 function HookCounter1(){
+
+  const [count, setCounter] = useState(0);
+  const [name, setName] = useState('');
   return (
-    
+
     <div>
     
     </div>
