32d30fde2503119b38a77f4b098c6cf32b5d5519
Godwin254 <godwinbee15@gmail.com>
2020-06-08 16:45:01 -0400

    imported axios library

--- components/HookFetch.js
+++ components/HookFetch.js
@@ -1,10 +1,11 @@
 //fetching data with UseEffect
 import React, {UseEffect, useState} from 'react'
+import axios from 'axios'
 
 function HookFetch(){
 
   const [posts, setPost] = useState([]);
-  
+
 
   return (
     <div>
