f8a74f5338870f467f7e285a8c64e36701485f97
Godwin254 <godwinbee15@gmail.com>
2020-06-08 16:43:01 -0400

    defined jsx

---
+++ components/HookFetch.js
@@ -0,0 +1,15 @@
+//fetching data with UseEffect
+import React, {UseEffect, useState} from 'react'
+
+function HookFetch(){
+
+  return (
+    <div>
+      <ul>
+        {
+          
+        }
+      </ul>
+    </div>
+  )
+}
