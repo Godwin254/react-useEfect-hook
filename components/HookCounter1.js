821cd43208dcebb393845d19145e34a5f10fe244
Godwin254 <godwinbee15@gmail.com>
2020-06-08 15:46:56 -0400

    add jsx files

--- components/HookCounter1.js
+++ components/HookCounter1.js
@@ -15,6 +15,13 @@ function HookCounter1(){
   return (
 
     <div>
+      <input 
+        type='text'
+        value={name}
+        onChange={(e) =>setName(e.target.value)}
+      />
+
+      <button type='button' onClick = {() => setCounter(count + 1)}>Click {count} times</button>
     
     </div>
   )
